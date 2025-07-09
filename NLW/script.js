fe2187f2f9baaedf5d00e70077101ccdd72bf6d5
lisboathecoder <gustavo.lisboa2009@gmail.com>
2025-07-09 15:15:49 -0300

    Refactor form handling and enhance button states for better user experience

--- NLW/script.js
+++ NLW/script.js
@@ -5,9 +5,34 @@ const askButton = document.getElementById('askButton');
 const form = document.getElementById('form');
 const aiResponse = document.getElementById('aiResponse');
 
-const enviarFormulario = (event) => {
-    event.preventDefault();
-    console.log(event)
-}
+const askAI = async
 
-form.addEventListener('submit', enviarFormulario)
+const sendForm = (event) => {
+    event.preventDefault();
+    const apiKey = apiKeyinput.value;
+    const game = gameSelect.value;
+    const question = questionInput.value;
+
+    if (apiKey == '' || game == '' || question == '') {
+        alert('Please fill in all fields.');
+        return;
+    }
+
+    askButton.disabled = true;
+    askButton.textContent = 'Asking...';
+    askButton.class.add('loading');
+
+    try {
+        // pergunta para a AI
+    } catch (error) {
+        console.error('Error:', error);
+        alert('An error occurred while processing your request. Please try again later.');
+        return;
+    } finally {
+        askButton.disabled = false;
+        askButton.textContent = 'Ask';
+        askButton.classList.remove('loading');
+    }
+
+form.addEventListener('submit', sendForm)
+}
